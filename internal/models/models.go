dcd1c88bf395c9c63642c5d41b84828d3d3c3801
Ankit Mishra <ankit1703@gmail.com>
2026-07-08 19:50:03 +0530

    Added index.

--- internal/models/models.go
+++ internal/models/models.go
@@ -4,22 +4,22 @@ import "time"
 
 type Order struct {
 	ID         string      `gorm:"type:uuid;default:gen_random_uuid();primaryKey" json:"id"`
-	UserID     string      `gorm:"not null" json:"user_id"`
-	Status     string      `gorm:"default:'pending'" json:"status"`
+	UserID     string      `gorm:"index;not null" json:"user_id"`
+	Status     string      `gorm:"index;default:'pending'" json:"status"`
 	TotalPrice float64     `json:"total_price"`
 	Items      []OrderItem `gorm:"foreignKey:OrderID;constraint:OnDelete:CASCADE;" json:"items"`
-	CreatedAt  time.Time   `gorm:"autoCreateTime" json:"created_at"`
+	CreatedAt  time.Time   `gorm:"autoCreateTime;index" json:"created_at"`
 	UpdatedAt  time.Time   `gorm:"autoUpdateTime" json:"updated_at"`
 }
 
 type OrderItem struct {
 	ID          string `gorm:"type:uuid;default:gen_random_uuid();primaryKey" json:"id"`
-	OrderID     string `gorm:"not null" json:"order_id"`
-	ProductID   string `gorm:"not null" json:"product_id"`
+	OrderID     string `gorm:"index;not null" json:"order_id"`
+	ProductID   string `gorm:"index;not null" json:"product_id"`
 	Quantity    int    `gorm:"not null" json:"quantity"`
 	ProductName string
 	Category    string
 	Price       float64   `gorm:"not null" json:"price"`
-	CreatedAt   time.Time `gorm:"autoCreateTime" json:"created_at"`
+	CreatedAt   time.Time `gorm:"autoCreateTime;index" json:"created_at"`
 	UpdatedAt   time.Time `gorm:"autoUpdateTime" json:"updated_at"`
 }
